135efcf1537f5fd18a924cea953621ecb176b5cf
arelymartinez2022 <armartinez@tracylc.net>
2022-10-18 11:54:16 -0700

    push

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//Arely Martinez 
+//October 18, 2022 11:25 AM
+//This program will ask the user if 
 Console.WriteLine("Hello, World!");
 Console.Clear();
 bool playAgain = true;
@@ -7,35 +10,39 @@ while(playAgain){
     bool isConverted = true;
     bool isNumber; 
     
-    while (isConverted == true){
-        Console.WriteLine("WHAT UR NAME? ");
-        string name = Console.ReadLine();
-        Console.WriteLine(" ");
 
-        if (string.IsNullOrEmpty(name)){
-            isConverted = false;
-            Console.WriteLine("Invaild Input, please try again ");
-        } else  {
-            Console.WriteLine("Vaild entry ");
-            Console.WriteLine( " " );
-            Console.WriteLine( "GOOD EVENING " + name );
-            break;
-        }
-    }
-
+    Console.WriteLine("WHAT UR NAME? ");
+    string name = Console.ReadLine();
+    Console.WriteLine(" ");
+    Console.WriteLine( "GOOD EVENING " + name );
     
     Console.WriteLine(" ");
     Console.WriteLine("Would you like to play again? Yes or No");
     string rePlay = Console.ReadLine();
-    if(rePlay == "YES" || rePlay == "Yes" || rePlay == "yes"){
-        playAgain = true;
-        Console.Clear();
-        Console.WriteLine( "LETS PLAY AGAIN!!" );
-        Console.WriteLine( " " );
-    } 
-    if(rePlay == "NO" || rePlay == "no"){
-        playAgain = false;
-         Console.WriteLine(" ");
-        Console.WriteLine( "THANK YOU FOR PLAYING!!! " );
+
+    bool converted = true;
+    while (converted){
+        if(rePlay == "YES" || rePlay == "Yes" || rePlay == "yes"){
+            playAgain = true;
+            converted = false;
+            Console.Clear();
+            Console.WriteLine( "LETS PLAY AGAIN!!" );
+            Console.WriteLine( " " );
+            break;
+        } 
+        if(rePlay == "NO" || rePlay == "no"){
+            playAgain = false;
+            converted = false;
+            Console.WriteLine(" ");
+            Console.WriteLine( "THANK YOU FOR PLAYING!!! " );
+            break;
+        }
+        else{
+            converted = true;
+            Console.WriteLine(" ");
+            Console.WriteLine( "INVAILD RESPONSE TRY AGAIN!!" );
+            Console.WriteLine(" ");
+            break;
+        }
     } 
 }
